984bc976cbd4bdb2582ce7a6d3a6c882fa9cc890
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-20 11:34:37 -0300

    Types of Events and Event Handlers

--- script.js
+++ script.js
@@ -131,29 +131,47 @@ logo.clasName = 'jonas';
 
 */
 
+///////////// Implementing Smooth Scrolling
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
-    const s1coords = section1.getBoundingClientRect(); // DOM rectangle
-    console.log(s1coords);
+    //// New way of scrolling
+    section1.scrollIntoView({ behavior: 'smooth' });
 
-    console.log(e.target.getBoundingClientRect());
+    //// Old scholl scrolling way
+    // const s1coords = section1.getBoundingClientRect(); // DOM rectangle
+    // console.log(s1coords);
 
-    console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+    // console.log(e.target.getBoundingClientRect());
 
-    console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+    // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-    // Scrolling
+    // console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
     // window.scroll(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
 
-    // Old scholl scrolling way
     // window.scrollTo({
     //     left: s1coords.left + window.pageXOffset,
     //     top: s1coords.top + window.pageYOffset,
     //     behavior: 'smooth'
     // });
-
-    // New way of scrolling
-    section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+/*
+///////////////////////////////////////
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+    alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+*/
