692c61849dc7823f55572aa0026c0271596d26fc
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-22 12:27:54 -0300

    Revealing Elements on Scroll

--- script.js
+++ script.js
@@ -139,6 +139,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+///////////// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target); // For increase performance
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15 // Section will be revealed when it is 15% visible
+});
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
